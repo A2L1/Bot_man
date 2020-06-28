4e813a15c6352be4caa705aac80f6d2974caea1c
A2L1 <pro.arossignol@gmail.com>
2020-06-28 20:28:09 +0200

    Modification du nom dico en source

---
+++ source.py
@@ -0,0 +1,3 @@
+
+dico = {'man':"```Ce bot vous servira à obtenir toutes les commandes concernant un bot. Pour obtenir la liste de ses commandes écrivez $man [Nom du bot voulu]```",
+'cleanchat':'```Ce bot sert à supprimer des messages dans un salon choisi.```',}
